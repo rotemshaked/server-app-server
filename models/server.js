31c4cc92ba90d36d81b083fb772676244224d9b4
rotemshaked <rotemshaked8@gmail.com>
2022-05-26 03:15:37 +0300

    connection to mongoDB

--- models/server.js
+++ models/server.js
@@ -1,5 +1,31 @@
-const { type, unique } = require("express/lib/response");
+const { type, required } = require("express/lib/response");
 const mongoose = require("mongoose");
+
+// const server = mongoose.Schema({
+//   ipAddress: {
+//     type: String,
+//     unique: true,
+//     required: true,
+//   },
+//   name: {
+//     type: String,
+//     unique: true,
+//     required: true,
+//   },
+//   type: { type: mongoose.Types.ObjectId, ref: "Types", required: true },
+//   isRunning: {
+//     type: Boolean,
+//     default: false,
+//   },
+//   startingDate: {
+//     type: Date,
+//   },
+//   endingDate: {
+//     type: Date,
+//   },
+//   sumToPay: { type: String, default: 0 },
+//   deleted: { type: Boolean, default: false },
+// });
 
 const server = mongoose.Schema({
   ipAddress: {
